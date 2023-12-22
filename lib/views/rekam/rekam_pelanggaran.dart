54274b861a76f175cb03a566d846b0fd126ebc17
annasbachtiar <anasbachtiar801@gmail.com>
2023-12-22 10:38:09 +0700

    add rekam form

--- lib/views/rekam/rekam_pelanggaran.dart
+++ lib/views/rekam/rekam_pelanggaran.dart
@@ -6,13 +6,112 @@ class RekamPelanggaranPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color(0xFFF6F6F6),
       appBar: AppBar(
+        backgroundColor: const Color(0xFFF6F6F6),
         centerTitle: true,
         title: const Text(
           'Rekam Pelanggaran',
           style: TextStyle(
             fontWeight: FontWeight.bold,
             fontSize: 30
+          ),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Form(
+          child: Padding(
+            padding: const EdgeInsets.all(20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  height: 150,
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.grey.withOpacity(0.2)),
+                  ),
+                  child: const Center(
+                    child: Text('Masukkan Gambar'),
+                  ),
+                ),
+        
+                //===== FORM INPUT FIELD =====//
+                const SizedBox(height: 20),
+                const Text('Batalyon', textAlign: TextAlign.left, style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+                const SizedBox(height: 5),
+                TextFormField(
+                  enabled: false,
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: const Color(0xFFEEEEEE),
+                    hintText: 'Pilih Batalyon',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10), // Adjust the radius as needed
+                      borderSide: BorderSide.none, // Remove the underline
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                const Text('Nama Taruna', textAlign: TextAlign.left, style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+                const SizedBox(height: 5),
+                TextFormField(
+                  enabled: false,
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: const Color(0xFFEEEEEE),
+                    hintText: 'Pilih Taruna',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10), // Adjust the radius as needed
+                      borderSide: BorderSide.none, // Remove the underline
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                const Text('Jenis Pelanggaran', textAlign: TextAlign.left, style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+                const SizedBox(height: 5),
+                TextFormField(
+                  enabled: false,
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: const Color(0xFFEEEEEE),
+                    hintText: 'Pilih Jenis Pelanggaran',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10), // Adjust the radius as needed
+                      borderSide: BorderSide.none, // Remove the underline
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                const Text('Petugas', textAlign: TextAlign.left, style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+                const SizedBox(height: 5),
+                TextFormField(
+                  enabled: false,
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: const Color(0xFFEEEEEE),
+                    hintText: 'Tony Stark',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10), // Adjust the radius as needed
+                      borderSide: BorderSide.none, // Remove the underline
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 40),
+                SizedBox(
+                  height: 56,
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    onPressed: () {}, 
+                    style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                      foregroundColor: Colors.white,
+                      backgroundColor: const Color(0xFF16192C)
+                    ),
+                    child: const Text('SUBMIT', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
